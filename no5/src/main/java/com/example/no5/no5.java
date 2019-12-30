3c14db6b368b5d81af0c9cb806df8fb0c7bce049
3a513120 <57247605+3a513120@users.noreply.github.com>
2019-12-31 04:10:37 +0800

    Add files via upload

---
+++ no5/src/main/java/com/example/no5/no5.java
@@ -0,0 +1,14 @@
+package com.example.no5;
+
+import java.util.Scanner;
+
+public class no5 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Conversion conversion = new Conversion();
+        conversion.DecimalConversion();
+        conversion.BinaryConversion();
+        conversion.HexadecimalConversion();
+
+    }
+}
